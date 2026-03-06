38c87fe02360b94d0dbeae4fe42db0c8abffeba0
Tiago-Fonteles <tiagofonteles@edu.unifor.br>
2026-03-06 10:26:29 -0300

    Update BEE1038.js

--- beecrowd/BEE1038.js
+++ beecrowd/BEE1038.js
@@ -1,1 +1,29 @@
+let codproduto;
+let precoprod;
+let qtd;
+let total;
+// entrada de dados
+let inputcod = prompt("Digite codigo: ");
+let qtdinput = prompt("Digite quantidade: ");
 
+// procesamento
+// converter entrada para numérico
+codproduto = parseInt(inputcod);
+qtd = parseInt(qtdinput);
+
+//verificar total a pagar
+if (codproduto == 1){
+   qtd = 4
+}
+if (codproduto == 2){
+    total = qtd * 4;
+}
+if (codproduto == 3){
+    total = qtd * 4;
+}
+if (codproduto == 4){
+    total = qtd * 4;
+}
+if (codproduto == 5){
+    total = qtd * 4;
+}
